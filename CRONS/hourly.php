7c43a6f51399a563900ed1c13426a4e1ef450314
Jason Millis <jas-n@outlook.com>
2018-03-24 11:26:42 +0000

    mystravarank.com

--- CRONS/hourly.php
+++ CRONS/hourly.php
@@ -11,7 +11,7 @@ $day	=date('d');
 $hour	=date('G');
 if(in_array($hour,array(0,6,12,18))){
 	# Collate data for 50 lowest `updatees`
-	if($athletes_=$db->query("SELECT * FROM `athletes` ORDER BY `updates` ASC LIMIT 50")){
+	if($athletes_=$db->query("SELECT * FROM `athletes` ORDER BY `updates` ASC LIMIT 1")){ # Limit 50
 		foreach($athletes_ as $athlete){
 			$strava->setAccessToken($athlete['access_token']);
 			$data=$strava->get('athletes/'.$athlete['strava_id'].'/stats');
