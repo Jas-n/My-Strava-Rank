7dff640ecf354b7ba654241823705442b2784e4c
Jason Millis <jas-n@outlook.com>
2019-02-09 23:46:46 +0000

    athlete list

--- js/athletes.js
+++ js/athletes.js
@@ -9,6 +9,21 @@ var athletes={
 		});
 	},
 	got:function(json){
-		console.log(json);
+		var athlete={};
+		for(i=0;i<json.data.length;i++){
+			athlete=json.data[i];
+			console.log(athlete);
+			$('.ranks').append('<div class="card rank">'+
+				'<div class="rank-number">'+(i+1)+'</div>'+
+				'<h3>'+(athlete.username?athlete.username:athlete.first_name+' '+athlete.last_name[0])+'</h3>'+
+				'<div class="row">'+
+					'<div class="col-6">'+php.number_format(athlete.points)+'</div>'+
+					'<div class="col-6">'+athlete.rank+'</div>'+
+				'</div>'+
+				'<div class="progress">'+
+					'<div class="progress-bar" style="width:'+athlete.rank_percent+'%" role="progressbar" aria-valuenow="'+athlete.points+'" aria-valuemin="0" aria-valuemax="'+athlete.next_rank_points+'"></div>'+
+				'</div>'+
+			'</div>');
+		}
 	}
 };
