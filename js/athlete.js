5908c4bdea2d48dcbc70d9b5a89ed7e4090277e4
Jason Millis <jas-n@outlook.com>
2019-03-11 08:32:20 +0000

    Part-population of athete

--- js/athlete.js
+++ js/athlete.js
@@ -15,5 +15,21 @@ var athlete={
 		athlete.name=data.username?data.username:data.first_name+' '+data.last_name[0];
 		athlete.small=data.last_activity.formatted?data.last_activity.formatted:data.updated.formatted;
 		core.set_header(athlete);
+		var map={
+			athlete_points:	php.number_format(data.gamification.points),
+			athlete_rank:	data.gamification.rank,
+			first_name:		data.first_name,
+			msr_join:		php.formatted_date(data.added.raw),
+			strava_join:	php.formatted_date(data.strava_join.raw),
+			username:		athlete.name
+		};
+		console.log(data);
+		$('[data-athlete]').each(function(){
+			if(map[$(this).data('athlete')]){
+				$(this).text(map[$(this).data('athlete')]);
+			}else{
+				console.log($(this).data('athlete'));
+			}
+		});
 	}
 };
